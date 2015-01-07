cb391be368ecdf230c62f99090a1e8ef49f0a59c
Sudharshan <sudhar.ti@gmail.com>
2015-01-07 03:34:24 -0600

    Devise form fixed, login and register part complete

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,11 +2,11 @@ module StaticPagesHelper
   def resource_name
     :user
   end
- 
+
   def resource
     @resource ||= User.new
   end
- 
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
