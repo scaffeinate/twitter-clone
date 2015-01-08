c31b29f3d3f855d27ef7fb22307ffb640ba2e0ea
Sudharshan <sudhar.ti@gmail.com>
2015-01-08 06:44:34 -0600

    Scope in route to authenticate user, add sign out link in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'static_pages#index'
 
+  authenticated :user do
+    root to: "home#index", as: "home"
+  end
+  unauthenticated :user do
+    root 'static_pages#index'
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
