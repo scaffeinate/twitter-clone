2399d2540c87efe38196cc301f9c6b29b21b4832
Sudharshan <sudhar.ti@gmail.com>
2015-01-08 06:38:30 -0600

    Configure permitted params devise, validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
     :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :username, presence: true, length: { maximum: 20 }
 end
