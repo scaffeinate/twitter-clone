578928dbf17a5c2c844b0b666fc236fb403fd7b9
Sudharshan <sudhar.ti@gmail.com>
2015-01-12 01:13:29 -0600

    Add ajax loader and bind with delay, complete pagination

--- app/assets/javascripts/paginate.js
+++ app/assets/javascripts/paginate.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
   if($(".pagination").length) {
-    $(window).scroll(function(){
+    $(".pagination").hide();
+    $("#loader").removeClass("hidden");
+    $(window).bindWithDelay("scroll", function(){
       var url = $("a.next_page").attr("href");
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-       $.getScript(url);
+        $.getScript(url);
       }
-    });
+    }, 750);
   }
 });
