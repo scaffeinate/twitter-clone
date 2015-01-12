6480ef468e97acceb50f83553ae9b8b68b47533f
Sudharshan <sudhar.ti@gmail.com>
2015-01-12 01:49:33 -0600

    Closes #9, List tweets in timeline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @tweets = Tweet.paginate(page: params[:page]).order('created_at DESC')
+    @tweets = Tweet.where("user_id in (?) OR user_id = ?", current_user.friend_ids.join(', '), current_user).paginate(page: params[:page]).order('created_at DESC')
   end
 end
