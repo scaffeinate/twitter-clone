37afc61bbdba9a4c579a45802db68aa0d0540fa2
Sudharsanan <sudhar.ti@gmail.com>
2015-03-17 21:17:16 -0500

    Match hashtags and mentions using regex

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,14 @@ module ApplicationHelper
 
   def parse(content)
     #content.gsub(/(?:^|\W)@(\w+)/, link_to(" @#{'\1'}", '/users/\1'))
-    content.gsub(/(?:^|\W)@(\w+)/) do |match|
-      link_to(match, user_path(match.gsub('@', '')))
+    content = content.gsub(/(?:\s|^)(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/i) do |match|
+      link_to(match, '#', class: 'hashtags')
     end
+
+    content.gsub(/(?:^|\W)@(\w+)/) do |match|
+      link_to(match, user_path(match.gsub('@', '')), class: 'mention')
+    end
+
   end
 
   def fetch_bg_img
