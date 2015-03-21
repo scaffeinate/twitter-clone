7bbb87d9103fadd645f304747862fb6146b025f6
Sudharsanan <sudhar.ti@gmail.com>
2015-03-21 03:21:55 -0500

    Cleanup error messages block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+  def fetch_background
+    if user_signed_in?
+      '/assets/bg-pattern.jpg'
+    else
+      '/assets/bg-image.jpg'
+    end
+  end
+
   def parse(content)
     #content.gsub(/(?:^|\W)@(\w+)/, link_to(" @#{'\1'}", '/users/\1'))
     content = content.gsub(/(?:\s|^)(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/i) do |match|
@@ -8,15 +16,6 @@ module ApplicationHelper
 
     content.gsub(/(?:^|\W)@(\w+)/) do |match|
       link_to(match, user_path(match.gsub('@', '')), class: 'mention')
-    end
-
-  end
-
-  def fetch_bg_img
-    if user_signed_in?
-      '/assets/background_home.jpg'
-    else
-      '/assets/img-1.jpg'
     end
   end
 end
